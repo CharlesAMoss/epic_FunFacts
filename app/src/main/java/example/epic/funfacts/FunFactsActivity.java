813df6bc4c0c2917e719133ec0652b85a75ed21f
Charles Moss <student@epicodus.com>
2015-11-30 14:40:17 -0800

    add random facts

--- app/src/main/java/example/epic/funfacts/FunFactsActivity.java
+++ app/src/main/java/example/epic/funfacts/FunFactsActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 
 public class FunFactsActivity extends AppCompatActivity {
 
@@ -22,8 +24,29 @@ public class FunFactsActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can see through time.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the Sun to reach Earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built." };
+
                 // The button is clicked, so update the fact label with a new fact
-                String fact = "Ostriches can see through time.";
+                String fact = "";
+
+                // Randomly select a fact
+                Random randomGenerator = new Random(); // Construct a new Random number generator
+                int randomNumber = randomGenerator.nextInt(facts.length);
+                //Convert the randomNumber to a text fact
+
+                fact = facts[randomNumber];
+
+                // Update the lable with our dynamic fact
                 factLabel.setText(fact);
             }
         };
