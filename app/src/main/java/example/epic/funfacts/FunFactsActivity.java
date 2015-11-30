27be580ed34494661ebda8951e8e344307004e3e
Charles Moss <student@epicodus.com>
2015-11-30 12:31:01 -0800

    add listener

--- app/src/main/java/example/epic/funfacts/FunFactsActivity.java
+++ app/src/main/java/example/epic/funfacts/FunFactsActivity.java
@@ -2,6 +2,11 @@ package example.epic.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class FunFactsActivity extends AppCompatActivity {
@@ -10,6 +15,17 @@ public class FunFactsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // Declare our View variables assign the Views from the layout file
+        TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+        showFactButton.setOnClickListener(listener);
 
     }
 }
